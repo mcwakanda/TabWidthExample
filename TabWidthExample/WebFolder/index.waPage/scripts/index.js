8695276499b199232611d85f6dcc8ab6fb9bc3d0
Greg McCarvell <greg@gregmccarvell.com>
2013-08-23 16:29:58 -0400

    Initial commit

---
+++ TabWidthExample/WebFolder/index.waPage/scripts/index.js
@@ -0,0 +1,31 @@
+﻿
+WAF.onAfterInit = function onAfterInit() {// @lock
+
+// @region namespaceDeclaration// @startlock
+	var button2 = {};	// @button
+	var allWidthsButton = {};	// @button
+// @endregion// @endlock
+
+// eventHandlers// @lock
+
+	button2.click = function button2_click (event)// @startlock
+	{// @endlock
+		$("#menuBar1").width("500px");
+		$("#menuItem1").width("100px");
+		$("#menuItem2").width("200px");
+		$("#menuItem3").width("100px");
+
+
+	};// @lock
+
+	allWidthsButton.click = function allWidthsButton_click (event)// @startlock
+	{// @endlock
+		$("#menuBar1").width("500px");
+		$("#tabView1 li").width("150px");
+	};// @lock
+
+// @region eventManager// @startlock
+	WAF.addListener("button2", "click", button2.click, "WAF");
+	WAF.addListener("allWidthsButton", "click", allWidthsButton.click, "WAF");
+// @endregion
+};// @endlock
